ec79b5654497b471dc15fe2459a891f7d2c397da
Nikhil5320 <53870479+Nikhil5320@users.noreply.github.com>
2019-09-20 18:46:25 +0530

    Update que.c

--- que.c
+++ que.c
@@ -19,9 +19,14 @@ int fr(int *front,int a[])
 {
  return a[*front];
 }
+void display(int a[],int *front,int *rear)
+{
+  for(int i=*front;i<=(*rear);i++)
+    printf("%d ",a[i]);
+}
 void enque(int x,int a[],int *front,int *rear)
 {
- if(*front==0 && *rear==(max-1))
+ if(*rear==(max-1))
   {
    printf("Q is full\n");
    return;
@@ -55,15 +60,23 @@ void main()
  int a[max],f,*front=-1,*rear=-1,n=0,x;
 while(n<4)
  {
-  printf("Enter 1: for enque,2: for deque,3: for to know the front element, 4 :for to exit the process\n");
+  printf("\nEnter 1: for enque,2: for deque,3: for to know the front element, 4 :for to exit the process\n");
   scanf("%d",&n);
   switch(n)
     {
      case 1 :  printf("enter the element to be added to the Q\n");
               scanf("%d",&x);
               enque(x,a,&front,&rear);
+              printf("now the que is : ");
+              display(a,&front,&rear);
+              printf("\n");
               break;
      case 2 : deque(a,&front,&rear);
+              f=fr(&front,a);
+              printf("the element %d is removed\n",f);
+              printf("now the que is : ");
+              display(a,&front,&rear);
+              printf("\n");
              break;
      case 3 : f=fr(&front,a);
              printf("The front element= %d\n",f);
